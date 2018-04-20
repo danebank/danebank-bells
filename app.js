9f81607c300157b734a9de95250694d28d4b0c13
krt2311 <kevin.turner@danebank.nsw.edu.au>
2018-04-20 10:14:35 +1000

    Added html frontend and bells schedule

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ const express = require('express');
 const morgan = require('morgan');
 const schedule = require('node-schedule');
 
+// initialise app
+const app = express();
+const loopSeconds = 20;
+const port = '3000';
 var player = require('play-sound')(opts = {});
+var evacSound;
+var evacOn = false;
 var sched = [];
-
-/*var bells = [
-	['5 * * * * *','./resources/sound.mp3'],
-	['10 * * * * *', './resources/mbeernut.mp3'],
-];*/
 
 var bells = [
 	['50 8 * * 1-5','./resources/sound.mp3'],
 	['10 9 * * 1-5', './resources/mbeernut.mp3'],
-	['50 9 * * 1-5', './resources/mbeernut.mp3'],
+	['50 9 * * 1-5', './resources/sound.mp3'],
 	['30 10 * * 1-5', './resources/mbeernut.mp3'],
-	['50 10 * * 1-5', './resources/mbeernut.mp3'],
+	['50 10 * * 1-5', './resources/sound.mp3'],
 	['30 11 * * 1-5', './resources/mbeernut.mp3'],
-	['10 12 * * 1-5', './resources/mbeernut.mp3'],
+	['10 12 * * 1-5', './resources/sound.mp3'],
 	['50 12 * * 1-5', './resources/mbeernut.mp3'],
-	['50 13 * * 1-5', './resources/mbeernut.mp3'],
+	['50 13 * * 1-5', './resources/sound.mp3'],
 	['30 14 * * 1-5', './resources/mbeernut.mp3'],
-	['15 15 * * 1-5', './resources/mbeernut.mp3']
+	['15 15 * * 1-5', './resources/sound.mp3']
 ];
 
 
@@ -37,11 +38,39 @@ sched = schedule.scheduleJob(bells[8][0], function(){ player.play(bells[8][1], {
 sched = schedule.scheduleJob(bells[9][0], function(){ player.play(bells[9][1], { timeout: 300 }, function(err){ if (err) throw err }) });
 sched = schedule.scheduleJob(bells[10][0], function(){ player.play(bells[10][1], { timeout: 300 }, function(err){ if (err) throw err }) });
 
+// main page
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
 
-/*for (var i=0; i<2; i++) {
-	sched[i] = schedule.scheduleJob(bells[i][0], function(){
-		player.play(String(bells[i][1]), { timeout: 300 }, function(err){
-  			if (err) throw err
-		})
+// sound off form post
+app.post('/evac', function(req, res) {
+	evacOn = true;
+	soundEvac();
+	res.sendFile(__dirname + '/index.html');
+});
+
+// all sounds off form post
+app.post('/', function(req, res) {
+	evacOn = false;
+	res.sendFile(__dirname + '/index.html');
+	evacSound.kill();
+});
+
+// port listener
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+function mainLoop() {
+	if (evacOn == true) { evacSound.kill(); soundEvac(); }
+	setTimeout(mainLoop, loopSeconds * 1000);
+}
+
+function soundEvac() {
+	evacSound = player.play('./resources/evac.mp3', { timeout: 300 }, function(err){
+  			if (err && !err.killed) throw err
 	});
-}*/
+}
+
+mainLoop();
